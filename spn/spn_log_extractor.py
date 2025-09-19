a1c7201ebb5b3f87e900ef5fe3b6bc8a93a845f2
Daniel Ribeiro Trindade <danielrtrindade1@gmail.com>
2025-09-19 19:04:29 -0300

    extrator spn em andamento

--- spn/spn_log_extractor.py
+++ spn/spn_log_extractor.py
@@ -1,3 +1,4 @@
+import csv
 import os
 from datetime import datetime
 import time
@@ -25,7 +26,58 @@ class Round:
 class Experiment:
     def __init__(self):
         self.server = Server()
-        self.clients = {}
+        self.clients = []
+
+    def export_csv(self, path):
+        self.save_server_csv(path)
+        self.save_clients_csv(path)
+
+    def save_server_csv(self, path):
+        data_csv = []
+        data_coluns = ['ROUND', 'ROUND_DURATION', 'T_SELECT', 'T_RETURN', 'T_AGGREG', 'T_SAVE', 'T_COMPUTE' ]
+        data_csv.append(data_coluns)
+        server_rounds = self.server.rounds
+        for server_round in server_rounds:
+            round_id = server_round.round_id
+            round_duration = server_round.round_duration
+            t_select = abs(server_round.events['T_SELECT_END'][0] - server_round.events['T_SELECT_START'][0])
+            t_return_0 = abs(server_round.events['T_RETURN_0_END'][0] - server_round.events['T_RETURN_0_START'][0])
+            t_return_1 = abs(server_round.events['T_RETURN_1_END'][0] - server_round.events['T_RETURN_1_START'][0])
+            t_return = t_return_0 + t_return_1
+            t_aggreg = abs(server_round.events['T_AGGREG_END'][0] - server_round.events['T_AGGREG_START'][0])
+            t_save = abs(server_round.events['T_SAVE_END'][0] - server_round.events['T_SAVE_START'][0])
+            t_compute = round_duration - t_select - t_return - t_aggreg - t_save
+            data_csv.append([round_id, round_duration, t_select, t_return, t_aggreg, t_save, t_compute])
+
+        csv_file_path = os.path.join(path, 'server.csv')
+        with open(csv_file_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(data_csv)
+
+    def save_clients_csv(self, path):
+        data_csv = []
+        data_coluns = ['ROUND', 'CLIENT_ID', 'ROUND_DURATION', 'T_SELECT', 'SELECTED', 'T_SEND', 'T_TRAIN', 'TRAINED', 'T_RETURN' ]
+        data_csv.append(data_coluns)
+        for client in self.clients:
+            client_id = client.client_id
+            for client_round in client.rounds:
+                round_id =  client_round.round_id
+                round_duration = abs(client_round.events['ROUND_END'][0] - client_round.events['ROUND_START'][0])
+                t_select = abs(client_round.events['T_SELECT'][0] - self.server.clients[client_id].rounds[round_id].events['T_SELECT'][0])
+                selected = client_round.events['T_SELECT'][1]
+                t_send = abs(client_round.events['T_SEND'][0] - self.server.rounds[round_id].events['T_SEND'][0])
+                t_train = client_round.events['T_TRAIN'][1]
+                trained = client_round.events['T_TRAIN'][0]
+                t_return_0 = abs(client_round.events['T_RETURN_0'][0] - self.server.clients[client_id].rounds[round_id].events['T_RETURN_0'][0])
+                t_return_1 = abs(client_round.events['T_RETURN_1'][0] - self.server.clients[client_id].rounds[round_id].events['T_RETURN_1'][0])
+                t_return = (t_return_0 + t_return_1) / 2
+                data_csv.append([round_id, client_id, round_duration, t_select, selected, t_send, t_train, trained, t_return])
+
+        csv_file_path = os.path.join(path, 'clients.csv')
+
+        with open(csv_file_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(data_csv)
 
 def process_log_line(line):
     timestamp_str, content = line.split(" - ", 1)
@@ -122,13 +174,13 @@ def read_spn_logs(spn_logs_path):
                         round_id = 0
                         round_time_init = 0
                         client = Client(client_id)
-                        experiment.clients[client_id] = client
+                        experiment.clients.append(client)
                         for line in f:
                             tag, timestamp, extras = process_log_line(line)
                             if tag == 'T_ARRIVAL':
                                 client.t_arrival = timestamp
                             if tag == 'START_ROUND':
-                                round_time_init = time.time()
+                                round_time_init =timestamp
                                 round_id = int(extras[0])
                                 client_round = Round(round_id)
                                 client.rounds.append(client_round)
@@ -144,7 +196,7 @@ def read_spn_logs(spn_logs_path):
                             if tag == 'T_RETURN_1':
                                 client.rounds[round_id].events[tag] = [timestamp]
                             if tag == 'END_ROUND':
-                                client.rounds[round_id].round_duration = time.time() - round_time_init
+                                client.rounds[round_id].round_duration = timestamp - round_time_init
 
     return experiment
 
