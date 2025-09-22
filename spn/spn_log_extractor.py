2ffbe83d4e12047b4c626f2ff0c0bfd5628fdc69
Daniel Ribeiro Trindade <danielrtrindade1@gmail.com>
2025-09-22 17:00:00 -0300

    debug extrator spn

--- spn/spn_log_extractor.py
+++ spn/spn_log_extractor.py
@@ -1,7 +1,7 @@
 import csv
 import os
+import sys
 from datetime import datetime
-import time
 
 
 class Client:
@@ -27,27 +27,41 @@ class Experiment:
     def __init__(self):
         self.server = Server()
         self.clients = []
+        self.server_metrics = {'ROUND_DURATION' : 0.0, 'T_SELECT' : 0.0, 'T_RETURN' : 0.0, 'T_AGGREG' : 0.0, 'T_SAVE' : 0.0, 'T_COMPUTE' : 0.0}
+        self.clients_metrics = {}
+        self.clients_metrics_avg = {'ROUND_DURATION' : 0.0, 'T_SELECT' : 0.0, 'T_SEND' : 0.0, 'T_TRAIN' : 0.0, 'T_RETURN' : 0.0}
 
-    def export_csv(self, path):
+    def save_spn_metrics(self, path):
         self.save_server_csv(path)
         self.save_clients_csv(path)
+        self.save_summary(path)
 
     def save_server_csv(self, path):
         data_csv = []
         data_coluns = ['ROUND', 'ROUND_DURATION', 'T_SELECT', 'T_RETURN', 'T_AGGREG', 'T_SAVE', 'T_COMPUTE' ]
         data_csv.append(data_coluns)
         server_rounds = self.server.rounds
+        n_rounds = len(server_rounds)
         for server_round in server_rounds:
             round_id = server_round.round_id
             round_duration = server_round.round_duration
-            t_select = abs(server_round.events['T_SELECT_END'][0] - server_round.events['T_SELECT_START'][0])
-            t_return_0 = abs(server_round.events['T_RETURN_0_END'][0] - server_round.events['T_RETURN_0_START'][0])
-            t_return_1 = abs(server_round.events['T_RETURN_1_END'][0] - server_round.events['T_RETURN_1_START'][0])
+            self.server_metrics['ROUND_DURATION'] += round_duration
+            t_select = abs((server_round.events['T_SELECT_END'][0] - server_round.events['T_SELECT_START'][0]).total_seconds())
+            self.server_metrics['T_SELECT'] += t_select
+            t_return_0 = abs((server_round.events['T_RETURN_0_END'][0] - server_round.events['T_RETURN_0_START'][0]).total_seconds())
+            t_return_1 = abs((server_round.events['T_RETURN_1_END'][0] - server_round.events['T_RETURN_1_START'][0]).total_seconds())
             t_return = t_return_0 + t_return_1
-            t_aggreg = abs(server_round.events['T_AGGREG_END'][0] - server_round.events['T_AGGREG_START'][0])
-            t_save = abs(server_round.events['T_SAVE_END'][0] - server_round.events['T_SAVE_START'][0])
+            self.server_metrics['T_RETURN'] += t_return
+            t_aggreg = abs((server_round.events['T_AGGREG_END'][0] - server_round.events['T_AGGREG_START'][0]).total_seconds())
+            self.server_metrics['T_AGGREG'] += t_aggreg
+            t_save = abs((server_round.events['T_SAVE_END'][0] - server_round.events['T_SAVE_START'][0]).total_seconds())
+            self.server_metrics['T_SAVE'] += t_save
             t_compute = round_duration - t_select - t_return - t_aggreg - t_save
+            self.server_metrics['T_COMPUTE'] += t_compute
             data_csv.append([round_id, round_duration, t_select, t_return, t_aggreg, t_save, t_compute])
+
+        for key in self.server_metrics.keys():
+            self.server_metrics[key] = self.server_metrics[key] / n_rounds
 
         csv_file_path = os.path.join(path, 'server.csv')
         with open(csv_file_path, 'w', newline='') as f:
@@ -60,24 +74,60 @@ class Experiment:
         data_csv.append(data_coluns)
         for client in self.clients:
             client_id = client.client_id
+            self.clients_metrics[client_id] = {'ROUND_DURATION' : 0.0, 'T_SELECT' : 0.0, 'T_SEND' : 0.0, 'T_TRAIN' : 0.0, 'T_RETURN' : 0.0}
+            n_rounds = len(client.rounds)
             for client_round in client.rounds:
                 round_id =  client_round.round_id
-                round_duration = abs(client_round.events['ROUND_END'][0] - client_round.events['ROUND_START'][0])
-                t_select = abs(client_round.events['T_SELECT'][0] - self.server.clients[client_id].rounds[round_id].events['T_SELECT'][0])
+                round_duration = client_round.round_duration.total_seconds()
+                self.clients_metrics[client_id]['ROUND_DURATION'] += round_duration
+                self.clients_metrics_avg['ROUND_DURATION'] += round_duration
+                t_select = abs((client_round.events['T_SELECT'][0] - self.server.clients[client_id].rounds[round_id].events['T_SELECT'][0]).total_seconds())
+                self.clients_metrics[client_id]['T_SELECT'] += t_select
+                self.clients_metrics_avg['T_SELECT'] += t_select
                 selected = client_round.events['T_SELECT'][1]
-                t_send = abs(client_round.events['T_SEND'][0] - self.server.rounds[round_id].events['T_SEND'][0])
-                t_train = client_round.events['T_TRAIN'][1]
-                trained = client_round.events['T_TRAIN'][0]
-                t_return_0 = abs(client_round.events['T_RETURN_0'][0] - self.server.clients[client_id].rounds[round_id].events['T_RETURN_0'][0])
-                t_return_1 = abs(client_round.events['T_RETURN_1'][0] - self.server.clients[client_id].rounds[round_id].events['T_RETURN_1'][0])
+                t_send = abs((client_round.events['T_SEND'][0] - self.server.rounds[round_id].events['T_SEND'][0]).total_seconds())
+                self.clients_metrics[client_id]['T_SEND'] += t_send
+                self.clients_metrics_avg['T_SEND'] += t_send
+                t_train = float(client_round.events['T_TRAIN'][2])
+                self.clients_metrics[client_id]['T_TRAIN'] += t_train
+                self.clients_metrics_avg['T_TRAIN'] += t_train
+                trained = client_round.events['T_TRAIN'][1]
+                t_return_0 = abs((client_round.events['T_RETURN_0'][0] - self.server.clients[client_id].rounds[round_id].events['T_RETURN_0'][0]).total_seconds())
+                t_return_1 = abs((client_round.events['T_RETURN_1'][0] - self.server.clients[client_id].rounds[round_id].events['T_RETURN_1'][0]).total_seconds())
                 t_return = (t_return_0 + t_return_1) / 2
+                self.clients_metrics[client_id]['T_RETURN'] += t_return
+                self.clients_metrics_avg['T_RETURN'] += t_return
                 data_csv.append([round_id, client_id, round_duration, t_select, selected, t_send, t_train, trained, t_return])
+
+            for client_key in self.clients_metrics.keys():
+                for key in self.clients_metrics[client_key].keys():
+                    self.clients_metrics[client_key][key] = self.clients_metrics[client_key][key] / n_rounds
+
+            n_clients = len(self.clients)
+            for client_metric in self.clients_metrics_avg:
+                self.clients_metrics_avg[client_metric] = self.clients_metrics[client_metric] / (n_rounds * n_clients)
 
         csv_file_path = os.path.join(path, 'clients.csv')
 
         with open(csv_file_path, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerows(data_csv)
+
+    def save_summary(self, path):
+        with open(os.path.join(path, 'summary.txt'), 'w', newline='') as f:
+            f.write(f'N_CLIENTS: {len(self.clients)}\n')
+            f.write(f'N_ROUNDS: {len(self.server.rounds)}\n\n')
+            f.write(f'SERVER METRICS:\n')
+            for server_metric in self.server_metrics:
+                f.write(f'\t{server_metric}: {self.server_metrics[server_metric]}\n')
+            f.write(f'\nCLIENT METRICS:\n')
+            for client_metric in self.clients_metrics_avg:
+                f.write(f'\t{client_metric}: {self.clients_metrics_avg[client_metric]}\n')
+            f.write(f'\nCLIENT METRICS BY CLIENT:\n')
+            for client in self.clients_metrics:
+                f.write(f'\t{client}:\n')
+                for client_metric in self.clients_metrics[client]:
+                    f.write(f'\t{client_metric}: {self.clients_metrics[client][client_metric]}\n')
 
 def process_log_line(line):
     timestamp_str, content = line.split(" - ", 1)
@@ -200,3 +250,7 @@ def read_spn_logs(spn_logs_path):
 
     return experiment
 
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else print("correct use: python spn_log_extractor.py <path_to_logs>")
+    experiment = read_spn_logs(path)
+    experiment.save_spn_metrics(path)
